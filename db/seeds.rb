c70d119dc1d69f5f06a71d69b1b27d9cbf51d397
IshsGit <aishtahad@yahoo.com>
2022-12-05 09:37:47 -0500

    testing backend

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+ApplicationRecord.transaction do 
+    puts "Destroying tables..."
+    User.destroy_all
+  
+    User.create!(
+      name: 'kyle Zish', 
+      email: 'zish@amazon.io', 
+      password: 'password'
+    )
+  
+    puts "Done!"
+  end
