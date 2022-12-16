b89673d58b6fa5d1c1127ca907dcbc13efbd3bdd
IshsGit <aishtahad@yahoo.com>
2022-12-15 22:22:55 -0500

    app rb classic loader

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ module Amazish
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
+    config.autoloader = :classic
     module Backend
       class Application < Rails::Application
         # ...
