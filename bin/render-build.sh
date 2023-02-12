428ad0997b3a1ddbc28c09b8051242d4af04d05a
IshsGit <aishtahad@yahoo.com>
2023-02-12 16:04:12 -0500

    environement

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,7 +5,7 @@ set -o errexit
 
 npm run build
 bundle install
-rails db:drop
+RAILS_ENV=production rails db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 rails db:create
 rails db:migrate
 rails db:seed #if needed
--- config/environment.rb
+++ config/environment.rb
@@ -6,5 +6,3 @@ Rails.application.initialize!
 
 Jbuilder.key_format camelize: :lower
 Jbuilder.deep_format_keys true
-
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1
